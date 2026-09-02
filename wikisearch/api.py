b65a367c212a2637890557b1934f9020ead42ea4
Cooper <cooperchangoc@gmail.com>
2026-09-02 07:15:56 -0700

    Update search type and camel case Dropping "insource:" from the parameters of the api request, regular gsrsearch matches rendered text instead and therefore works better, changed function naming to camel case

--- wikisearch/api.py
+++ wikisearch/api.py
@@ -2,17 +2,13 @@ import urllib.request
 import urllib.parse
 import json
 
-
-# IMPORTANT: keep note of potential api response errors since will be making many requests, like error code ratelimited
-# Use | to combine multiple requests into 1? like titles=pageA|pageB|pageC
-
 class WikipediaAPI:
     def __init__(self):
-        self.baseUrl = 'https://en.wikipedia.org/w/api.php'
+        self.base_url = 'https://en.wikipedia.org/w/api.php'
         self.headers = {'User-Agent': 'Wiki-Search-VideoBot/0.1 (github.com/Orbit274/wiki-search-video)'}
 
-    def baseParams(self, word: str) -> dict:
-        return {'action': 'query', 'generator': 'search', 'gsrsearch': f'insource:"{word}"', 'gsrlimit': 20, 'prop': 'info', 'inprop': 'url', 'format': 'json', 'formatversion': 2, 'pretty': 1}
+    def get_params(self, word: str) -> dict:
+        return {'action': 'query', 'generator': 'search', 'gsrsearch': f'"{word}"', 'gsrlimit': 20, 'prop': 'info', 'inprop': 'url', 'format': 'json', 'formatversion': 2}
 
     def request(self, params: dict) -> dict:
         url = self.base_url + "?" + urllib.parse.urlencode(params)
